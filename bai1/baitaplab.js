3b89a58cb42db3f4b4a33c1f218d3b7c3398af2c
ITPLUS Academy <thanghoang064@gmail.com>
2023-03-25 14:28:20 +0700

    update bai lab

---
+++ bai1/baitaplab.js
@@ -0,0 +1,11 @@
+// tạo 1 class SoHoc gồm 1 thuộc tính so
+// và các phương thức sau:
+// phương thức kiểm tra xem thuộc tính số đó có phải là số chẵn  hay số lẻ 
+// phương thức tính giai thừa của số đó ví dụ so = 5 . 5! = 1*2*3*4*5
+// phương thức kiểm tra xem số đó có phải là số nguyên tố hay không
+//(Số nguyên tố là số chia hết cho 1 và chính nó)
+// ví dụ 5 là số nguyên tố ví 5 chỉ chia hết có 1 và 5
+//phương thức kiểm tra xem số đó có phải là số hoàn hảo hay ko 
+// số hoàn hảo là số có tổng các ước của nó trừ nó bằng chính nó 
+// ví dụ 28 là số hoàn hảo vì tổng
+//các ước của 28 là 1 + 2 + 4 + 7 + 14 = 28
