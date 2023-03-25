90ebeb5da2e2de9e88305808b14efb1ac3c709e5
ITPLUS Academy <thanghoang064@gmail.com>
2023-03-25 14:39:04 +0700

    update bai 2

--- bai1/baitaplab.js
+++ bai1/baitaplab.js
@@ -9,3 +9,18 @@
 // số hoàn hảo là số có tổng các ước của nó trừ nó bằng chính nó 
 // ví dụ 28 là số hoàn hảo vì tổng
 //các ước của 28 là 1 + 2 + 4 + 7 + 14 = 28
+
+// tạo 1 class SinhVien gồm các thuộc tính sau :
+// masv,tensv,namsinh,diemtoan,diemly,diemhoa
+//xây dựng phương thức sau :
+// tính tuổi  = năm hiện tại - năm sinh (có trả về)
+// tính điểm trung bình = (diemtoan + diemly+diemhoa)/3 (có trả về)
+// phương thức xếp loại :
+// nếu điểm tb => 6.5 trả về đạt 
+// nếu điểm tb < 6.5 trả về không đạt 
+
+//tạo 1 mảng đối tượng gồm 5 sinh viên
+//hiển thị lương các đối tượng sinh viên có tuổi > 15 và tuổi < 20 
+// đếm xem có bao nhiêu sinh viên đạt
+//tính tổng điểm trung bình
+// của các đối tượng sinh viên có tuổi > 15
